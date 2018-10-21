e0b1e9d510cb5df6bb77d86da0d76912a34d5228
Forrest Timour <timourf@cs.washington.edu>
2018-10-20 23:28:33 -0700

    added empty functions

--- django/project404/app/views.py
+++ django/project404/app/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 def index(request):
     return HttpResponse("This is a test page")
+
+def blackrockTest(request):
+    output = requests.get(
+            url="https://www.blackrock.com/tools/hackathon/performance",
+            params={
+                'identifiers' : 'AAPL',
+                #exclude unused fields
+                'startDate' : '20180101',
+                'returnsType' : 'MONTHLY',
+            })
+    return HttpResponse(output.success)
+
+def companyToTicker(company):
+    #TODO
+    return None
+
+def blackrockPerformance():
+    pass
+
+def blackrockPortfolio():
+    pass
+
+def blackrockSearchSecurities():
+    pass
+
+def blackrockSecurityData():
+    pass
 
 # Create your views here.
