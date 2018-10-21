399a9e6b4dd60115236a8902b16c5c697a59369b
Forrest Timour <timourf@cs.washington.edu>
2018-10-21 02:11:37 -0700

    added html page

--- django/project404/app/views.py
+++ django/project404/app/views.py
@@ -6,30 +6,37 @@ def index(request):
     return HttpResponse("This is a test page")
 
 def blackrockTest(request):
-    output = requests.get(
-            url="https://www.blackrock.com/tools/hackathon/performance",
-            params={
-                'identifiers' : 'AAPL',
-                #exclude unused fields
-                'startDate' : '20180101',
-                'returnsType' : 'MONTHLY',
-            })
-    return HttpResponse(output.success)
+    ticker = companyToTicker('apple')
+    with open('app/page/pageBegin.html', 'r') as f:
+        data1 = f.read()
+    data2 = blackrockPerformance(ticker)
+    with open('app/page/pageMiddle1.html', 'r') as f:
+        data3 = f.read()
+    data4 = blackrockSearchSecurities(ticker)
+    with open('app/page/pageMiddle2.html', 'r') as f:
+        data5 = f.read()
+    data6 = blackrockSecurityData(ticker)
+    with open('app/page/pageEnd.html', 'r') as f:
+        data7 = f.read()
+    return HttpResponse(data1+data2+data3+data4+data5+data6+data7)
 
 def companyToTicker(company):
-    #TODO
-    return None
+    return 'AAPL'
 
-def blackrockPerformance():
-    pass
+def blackrockPerformance(ticker):
+    output = requests.get(
+            url="https://www.blackrock.com/tools/hackathon/performance",
+            params={ 'identifiers' : ticker })
+    return output.text
 
-def blackrockPortfolio():
-    pass
+def blackrockSearchSecurities(ticker):
+    output = requests.get(
+            url="https://www.blackrock.com/tools/hackathon/search-securities",
+            params={ 'identifiers' : ticker })
+    return output.text
 
-def blackrockSearchSecurities():
-    pass
-
-def blackrockSecurityData():
-    pass
-
-# Create your views here.
+def blackrockSecurityData(ticker):
+    output = requests.get(
+            url="https://www.blackrock.com/tools/hackathon/security-data",
+            params={ 'identifiers' : ticker })
+    return output.text
